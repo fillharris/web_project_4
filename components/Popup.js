7f4d4ef14fece8880e253105379b2b16fbe77127
Philip Harris <ptharris79@gmail.com>
2022-06-17 12:48:12 -0400

    Add Popup

--- components/Popup.js
+++ components/Popup.js
@@ -1,30 +1,52 @@
-
 class Popup {
-    constructor(popupSelector) {
-        this._popupElement = document.querySelector(`.${popupSelector}`);
-        this._handleEsc =  this._handleEsc.bind(this);
-        this._popup =  document.querySelector(".popup_open");
-    }
+  constructor(popupSelector) {
+    this._popupElement = document.querySelector(`.${popupSelector}`);
+    this._handleEsc = this._handleEsc.bind(this);
+    this._popup = document.querySelector(".popup_open");
+    this._popupSelector = popupSelector;
+    this._popups = Array.from(document.querySelectorAll(".popup"));
+  }
 
-    _handleEsc(evt) {
-        if (evt.key === "Escape") {
-            close(this._popup);
-          }
+  _handleEsc(evt) {
+    if (evt.key === "Escape") {
+      close(this._popup);
     }
+  }
 
-    setEventListeners() {
+  _handleClickOutsideToClose() {
+    this._popups.forEach((this) => {
+      this._popup.addEventListener("mousedown", (evt) => {
+        if (
+          evt.target.classList.contains("popup") ||
+          evt.target.classList.contains("popup__close-button")
+        ) {
+          close(this._popup);
+        }
+      });
+    });
+  }
 
-    }
+  setEventListeners() {
+    this._popupSelector.addEventListener("click", (evt) => {
+      if (
+        evt.target.classList.contains("popup") ||
+        evt.target.classList.contains("popup__close-button") ||
+        evt.target.classList.contains("popup_open")
+      ) {
+        this.close();
+      }
+    });
+  }
 
-    open() {
-       this._popup.classList.add("popup_open");
-        document.addEventListener("keydown", this._handleEsc);
-    }
+  open() {
+    this._popup.classList.add("popup_open");
+    document.addEventListener("keydown", this._handleEsc);
+  }
 
-    close() {
-        this._popup.classList.remove("popup_open");
-        document.removeEventListener("keydown", this._handleEsc);
-    }
+  close() {
+    this._popup.classList.remove("popup_open");
+    document.removeEventListener("keydown", this._handleEsc);
+  }
 }
 
 export default Popup;
