c8d6c914244035eb01f2a2bd1f297c23c73679d1
Philip <ptharris79@gmail.com>
2022-07-19 12:49:01 -0400

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html" 
+      template: "./src/index.html",
+      favicon: "./src/images/favicon.co" 
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
