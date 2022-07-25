a9122da6540ff7e7800e77abf3ab143523eb1a72
Philip <ptharris79@gmail.com>
2022-07-25 01:24:46 -0400

    Fix PopupWithForm

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,7 +6,7 @@ export default class PopupWithForm extends Popup {
     this._handleFormSubmit = handleFormSubmit;
     this._formList = [...this._popup.querySelectorAll(".popup__form")];
     this._formEl = this._popup.querySelector(".popup__form");
-    this._button = this._popup.querySelector(".popup__save-button");
+    this._button = this._popup.querySelector(".popup__button");
     this._buttonOriginalText = this._button.textContent;
   }
   // create and return an object from all the input boxes' answers
