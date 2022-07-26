1d6bb22bb3bc67567cb1025c6e9f1b0e846feca9
Philip <ptharris79@gmail.com>
2022-07-25 22:37:08 -0400

    Fix popup error

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,7 @@
 export const settings = {
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
-  inputErrorClass: "popup__error_type_error",
+  inputErrorClass: "popup__error",
   errorClass: "popup__error_visible",
 };
 export const cardsContainer = ".photo-grid__cards";
